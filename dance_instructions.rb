95b746e02ae1c8d2d6e031e3aac03988506fb877
Christina Ng <christinasy317@gmail.com>
2018-07-18 02:18:48 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,17 @@
 
 # Code your methods below
+def starting_stance
+  puts "Plant legs far apart"
+end
+
+def skip_step(starting_foot)
+  puts "Lower #{starting_foot} to the ground"
+  puts "Bounce #{starting_foot} back up slightly, kicking it a few inches back"
+  puts "Briefly raise the opposite foot, then the #{starting_foot} twice more!"
+end
+
+def base_footwork
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+
+end
