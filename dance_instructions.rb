956119199db361431f3b38ab46266d3d51b0602a
Christina Ng <christinasy317@gmail.com>
2018-07-18 02:22:22 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -22,3 +22,24 @@ def bob_the_reins
   puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
   puts "Do this 7 more times!"
 end
+
+def lasso
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts "Lift right arm up and point right elbow diagonally to the right."
+  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso."
+  puts "Do this 7 more times!"
+end
+
+def bust_a_move
+  puts "Be cool and be snazzy ;)"
+end
+
+def gangnam_style
+  starting_stance
+  base_footwork
+  skip_step("right foot")
+  bob_the_reins
+  skip_step("left foot")
+  lasso
+  bust_a_move
+end
