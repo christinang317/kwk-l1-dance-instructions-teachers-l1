dd45a92677a6fbbe897c683603ff17d21b7c701a
Christina Ng <christinasy317@gmail.com>
2018-07-18 02:20:45 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -15,3 +15,9 @@ def base_footwork
   puts "Return right foot to the ground"
   skip_step("left foot")
 end
+
+def bob_the_reins
+    puts "Holding your arms out in front of you, straight and at chest level."
+    puts "Cross your right wrist over your left and hold them together"
+    puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+end 
